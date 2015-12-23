761c998c3c7501052191d2381df2d52542f99c90
Heitor Dolinski <heitor.face@gmail.com>
2015-12-23 18:21:09 +0100

    better structure

--- createProject.php
+++ createProject.php
@@ -29,7 +29,7 @@ if(sizeof($_POST) > 0) {
 
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="assets/index.css">
+	<link rel="stylesheet" type="text/css" href="assets/style/index.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
--- index.php
+++ index.php
@@ -49,12 +49,12 @@ try {
 	<head>
 		<meta charset="utf-8">
 	    <title>Paymentwall Demo Page</title>
-		<link rel="stylesheet" type="text/css" href="assets/index.css">
+		<link rel="stylesheet" type="text/css" href="assets/style/index.css">
 	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 	    <script src="https://api.paymentwall.com/brick/brick.1.4.js"> </script>
-		<script src="pw/public/index.js"></script>
+		<script src="assets/js/index.js"></script>
 	</head>
 
 	<body>
@@ -67,7 +67,7 @@ try {
 				</a>
 			</div>
 
-			<img src="pw/public/pw.png" class="logo">
+			<img src="assets/img/pw.png" class="logo">
 		
 			<div class="paymentMethods">
 				<h2>Amount: 9.99 USD</h2>
